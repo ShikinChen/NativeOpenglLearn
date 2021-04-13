1e3e3fcda06ba5629f1a44418fab959c35568379
Shiki <pitatensai@163.com>
2021-04-13 09:14:26 +0800

    修正图片矩阵缩放显示

--- demo/src/demo/TextureMap.cpp
+++ demo/src/demo/TextureMap.cpp
@@ -15,47 +15,47 @@ TextureMap::~TextureMap() {
 }
 const char *TextureMap::GetVertex() {
   return "#version 300 es                          \n"
-         "layout(location = 0) in vec4 v_Position;  \n"
-         "layout(location = 1) in vec2 f_Position;  \n"
-         "out vec2 v_texCoord;  					\n"
-         "uniform mat4 u_Matrix;  				    \n"
-         "void main()                              \n"
-         "{                                        \n"
-         "   gl_Position = v_Position*u_Matrix;   \n"
-         "   v_texCoord = f_Position;              \n"
-         "}                                        \n";
+		 "layout(location = 0) in vec4 v_Position;  \n"
+		 "layout(location = 1) in vec2 f_Position;  \n"
+		 "out vec2 v_texCoord;  					\n"
+		 "uniform mat4 u_Matrix;  				    \n"
+		 "void main()                              \n"
+		 "{                                        \n"
+		 "   gl_Position = v_Position*u_Matrix;   \n"
+		 "   v_texCoord = f_Position;              \n"
+		 "}                                        \n";
 }
 const char *TextureMap::GetFragment() {
   return "#version 300 es                              \n"
-         "precision mediump float;                     \n"
-         "in vec2 v_texCoord;  							\n"
-         "layout(location = 0) out vec4 outColor;  		\n"
-         "uniform sampler2D s_TextureMap;  				\n"
-         "void main()                                  \n"
-         "{                                            \n"
-         "   outColor = texture(s_TextureMap, v_texCoord);  \n"
-         "}                                            \n";
+		 "precision mediump float;                     \n"
+		 "in vec2 v_texCoord;  							\n"
+		 "layout(location = 0) out vec4 outColor;  		\n"
+		 "uniform sampler2D s_TextureMap;  				\n"
+		 "void main()                                  \n"
+		 "{                                            \n"
+		 "   outColor = texture(s_TextureMap, v_texCoord);  \n"
+		 "}                                            \n";
 }
 bool TextureMap::OnCreate() {
   PLOGD << "OnCreate";
   if (!BaseShader::OnCreate()) {
-    return false;
+	return false;
   }
   float v[] = {
-      -1.0f, 0.5f, 0.0f,
-      -1.0f, -0.5f, 0.0f,
-      1.0f, -0.5f, 0.0f,
-      1.0f, 0.5f, 0.0f,
+	  -1.0f, 0.5f, 0.0f,
+	  -1.0f, -0.5f, 0.0f,
+	  1.0f, -0.5f, 0.0f,
+	  1.0f, 0.5f, 0.0f,
   };
   vertexsSize = sizeof(v) / sizeof(v[0]);
   vertexs = new float[vertexsSize];
   memcpy(vertexs, v, sizeof(v));
 
   float f[] = {
-      0.0f, 0.0f,
-      0.0f, 1.0f,
-      1.0f, 1.0f,
-      1.0f, 0.0f,
+	  0.0f, 0.0f,
+	  0.0f, 1.0f,
+	  1.0f, 1.0f,
+	  1.0f, 0.0f,
   };
   fragmentsSize = sizeof(f) / sizeof(f[0]);
   fragments = new float[fragmentsSize];
@@ -70,8 +70,8 @@ bool TextureMap::OnCreate() {
   glBindTexture(GL_TEXTURE_2D, GL_NONE);
 
   if (program > 0) {
-    s_TextureMap = glGetUniformLocation(program, "s_TextureMap");
-    u_Matrix = glGetUniformLocation(program, "u_Matrix");
+	s_TextureMap = glGetUniformLocation(program, "s_TextureMap");
+	u_Matrix = glGetUniformLocation(program, "u_Matrix");
   }
   return true;
 }
@@ -85,36 +85,36 @@ void TextureMap::OnDraw() {
   PLOGD << "OnDraw";
   BaseShader::OnDraw();
   if (img.ppPlane[0] && program > GL_NONE && textureId > GL_NONE) {
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.ppPlane[0]);
-    glBindTexture(GL_TEXTURE_2D, GL_NONE);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.ppPlane[0]);
+	glBindTexture(GL_TEXTURE_2D, GL_NONE);
 
-    glUseProgram(program);
+	glUseProgram(program);
 
-    GLuint v_index = 0;//对应layout的索引
-    glVertexAttribPointer(v_index, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), vertexs);
-    glEnableVertexAttribArray(v_index);
+	GLuint v_index = 0;//对应layout的索引
+	glVertexAttribPointer(v_index, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), vertexs);
+	glEnableVertexAttribArray(v_index);
 
-    GLuint f_index = 1;//对应layout的索引
-    glVertexAttribPointer(f_index, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), fragments);
-    glEnableVertexAttribArray(f_index);
+	GLuint f_index = 1;//对应layout的索引
+	glVertexAttribPointer(f_index, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), fragments);
+	glEnableVertexAttribArray(f_index);
 
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureId);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureId);
 
-    glUniform1i(s_TextureMap, 0);
+	glUniform1i(s_TextureMap, 0);
 
-    glUniformMatrix4fv(u_Matrix, 1, GL_FALSE, value_ptr(matrix));
+	glUniformMatrix4fv(u_Matrix, 1, GL_FALSE, value_ptr(matrix));
 
-    GLushort indices[] = {0, 1, 2, 0, 2, 3};
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
+	GLushort indices[] = {0, 1, 2, 0, 2, 3};
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
   }
 }
 void TextureMap::Destroy() {
   PLOGD << "Destroy";
   if (program > 0) {
-    glDeleteTextures(1, &textureId);
+	glDeleteTextures(1, &textureId);
   }
   BaseShader::Destroy();
 }
@@ -123,18 +123,21 @@ NativeImage *TextureMap::GetImg() {
 }
 void TextureMap::SetOrthoM() {
   if (img.width <= 0 || img.height <= 0 || width <= 0 || height <= 0) {
-    return;
+	return;
   }
   float sreen_r = width * 1.0 / height;
   float img_r = img.width * 1.0 / img.height;
-  if (sreen_r > img_r) {//宽度缩放
-    float r = width / (height * 1.0 / img.height * img.width);
-    matrix = glm::scale(mat4(1.0f), vec3(r, 1.0f, 1.0f));
-  } else {//高度缩放
-    float r = height / (width * 1.0 / img.width * img.height);
-    matrix = glm::scale(mat4(1.0f), vec3(1.0f, r, 1.0f));
-  }
 
+  float lr = 1.0f;
+  float tb = 1.0f;
+  if (sreen_r > img_r) {//宽度缩放
+	lr = width / (height * 1.0 / img.height * img.width);
+	//这里top和bottom为0.5对应回顶点坐标0.5
+	tb = 0.5f;
+  } else {//高度缩放
+	lr = height / (width * 1.0 / img.width * img.height);
+  }
+  matrix = glm::ortho(-lr, lr, -tb, tb);
 }
 
 
